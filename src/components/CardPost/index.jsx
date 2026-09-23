efaa58c39830cd9d9c5b4f49e970394f861801c8
Daniel Granato <dgmgranato@gmail.com>
2026-09-23 11:35:59 -0400

    set axios interceptor request

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -21,13 +21,7 @@ export const CardPost = ({ post }) => {
 
     const handleLikeButton = () => {
 
-        const token = localStorage.getItem('access_token')
-
-        http.post(`blog-posts/${post.id}/like`, {}, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
+        http.post(`blog-posts/${post.id}/like`)
             .then(() => {
                 setLikes(oldState => oldState + 1)
                 console.log('incrementar like')
